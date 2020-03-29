1e79255e1aadc94fd03ad1681611e3304bb8e1de
Imam Hambali <imamhambali@hotmail.co.id>
2020-03-29 19:19:23 +0700

    Add asset remove and mapping local data

--- app/src/main/java/com/android/themoviedb/viewmodel/DaoViewModel.kt
+++ app/src/main/java/com/android/themoviedb/viewmodel/DaoViewModel.kt
@@ -1,17 +1,25 @@
 package com.android.themoviedb.viewmodel
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
 import com.android.themoviedb.base.BaseViewModel
+import com.android.themoviedb.helper.Resource
+import com.android.themoviedb.model.MovieDetailAdapter
+import com.android.themoviedb.model.MovieDetailDatabase
 import com.android.themoviedb.model.MovieDetailResult
 import com.android.themoviedb.repository.DaoRepository
 
 class DaoViewModel(private val repository: DaoRepository) : BaseViewModel() {
+
+    private var data: LiveData<List<MovieDetailAdapter>> = repository.getAllData()
 
     fun storeDataMovie(data: MovieDetailResult): Boolean {
         repository.storeResponseData(data)
         return true
     }
 
-    fun getAllData() {
-        repository.getAllData()
+    fun getAllData(): LiveData<List<MovieDetailAdapter>> {
+        return data
     }
 }
